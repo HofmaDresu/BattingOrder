681bc3d49045933eb526eaa08773a7838aadd5d5
Matt Hoffman <hofmadresu@gmail.com>
2018-06-15 10:51:11 -0400

    Add prism code

--- BattingOrder/BattingOrder/App.xaml.cs
+++ BattingOrder/BattingOrder/App.xaml.cs
@@ -1,32 +1,22 @@
-using System;
-using Xamarin.Forms;
+using Prism.DryIoc;
+using Prism.Ioc;
 using Xamarin.Forms.Xaml;
 
-[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace BattingOrder
 {
-	public partial class App : Application
+    public partial class App : PrismApplication
 	{
-		public App ()
-		{
-			InitializeComponent();
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+            NavigationService.NavigateAsync($"{nameof(MainPage)}");
+        }
 
-			MainPage = new MainPage();
-		}
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+            containerRegistry.RegisterForNavigation<MainPage>(nameof(MainPage));
+        }
+    }
 }
---
+++ BattingOrder/BattingOrder/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattingOrder.ViewModels
+{
+    class MainPageViewModel
+    {
+    }
+}
